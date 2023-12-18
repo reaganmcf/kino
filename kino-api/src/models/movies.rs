4f590fd3d8381a8c374ad47d0e7da03582a258ed
Reagan McFarland <reagan.mcfarland1059@gmail.com>
2023-12-18 15:54:48 -0500

    move stuff around

---
+++ kino-api/src/models/movies.rs
@@ -0,0 +1,29 @@
+use chrono::{NaiveDate, NaiveDateTime};
+use diesel::prelude::*;
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Queryable, Selectable, Serialize)]
+#[serde(crate = "rocket::serde")]
+#[diesel(table_name = crate::schema::movies)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Movie {
+    pub id: i32,
+    pub title: String,
+    pub release_date: NaiveDate,
+    pub genre: Option<String>,
+    pub director: Option<String>,
+    pub description: Option<String>,
+    pub created_at: NaiveDateTime,
+    pub updated_at: NaiveDateTime,
+}
+
+#[derive(Insertable, Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+#[diesel(table_name = crate::schema::movies)]
+pub struct NewMovie<'a> {
+    pub title: &'a str,
+    pub release_date: NaiveDate,
+    pub genre: Option<&'a str>,
+    pub director: Option<&'a str>,
+    pub description: Option<&'a str>,
+}
